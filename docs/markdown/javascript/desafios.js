eb51c93fc3166acbc637a39603ea61e15c762a45
2sebass5 <2juanseortiz25@gmail.com>
2024-03-11 17:37:40 -0500

    commit

---
+++ docs/markdown/javascript/desafios.js
@@ -0,0 +1,12 @@
+"use strict"
+// Hacer un programa que salude
+/* console.log("hola")
+document.write("hola") */
+
+// Hacer prgorama que salude por nombre, si no recibe nombre solo saludar hola
+/* const nombreUsuario = prompt ("Ingrese su nombre");
+if (nombreUsuario) {
+  document.write("Hola, " + nombreUsuario + "!")
+} else {
+    document.write("hola") 
+}*/
--- docs/markdown/javascript/fundamentos.js
+++ docs/markdown/javascript/fundamentos.js
@@ -4,5 +4,128 @@ Este es un
 comentario de
 varias lineas
 */
-alert("mensaje")
+ 
+/* Metodos de interaccion */
+// Entrada
+/* let Entrada = prompt();
+entrada = prompt("Instrucciones");
+console.log(entrada); */
+//Salida
+/*console.log("mensaje en consola")
+document.write("nebsaje en interfaz de usuario")
+alert ("mensaje en ventana emergente") */
 
+/* VARIABLES */
+// Declaracion
+/* saludo1 = "hola1"
+var saludo2 = "hola2"
+let saludo3 = "hola3"
+const saludo4 = "hola4"
+console.log(saludo1)
+console.log(saludo2)
+console.log(saludo3)
+console.log(saludo4)*/
+
+//Ambito de las variables
+/* let variableGlobal = "soy variable global"
+console.log(variableGlobal)
+{
+    let variableLocal = "soy variable local"
+    console.log(variableLocal)
+} */
+
+/* cambia Valor = 1;
+const noCambiarValor = 2;
+cambiaValor ="1";
+noCambiaValor = 2;
+console.log (cambiaValor)
+console.log (noCambiarValor) */
+
+/*Modo estricto */
+"use strict"
+/*saludo = "hola";
+console.log(saludo); */
+
+/* Tipos de dato primitivos */
+// string
+let miVariable = "";
+ miVariable = "este es un texto de 'ejemplo1'";
+ miVariable = 'este es un texto de ejemplo 2';
+ miVariable = 'este es un texto de ejemplo 3';
+ // number
+ miVariable = 10;
+ miVariable = 9.5;
+ miVariable = 0xFF;
+  miVariable = 123e5
+// boolean
+  miVariable = true;
+  miVariable = false;
+ // undefined
+ let miVariableSinAsignacion;
+ //console.log(miVariableSinAsignacion);
+ // null
+ miVariable = null;
+ //console.log(miVariable);
+ 
+ /* Operador typeof */
+ let entradaUsuario = '123';
+ console.log(entradaUsuario);
+ console.log(typeof entradaUsuario);
+ entradaUsuario = 123;
+ console.log(entradaUsuario);
+ console.log(typeof entradaUsuario);
+
+/* Conversion de tipos */
+
+
+/* Expresiones booleanas */
+// Turthy o verdaderos
+/*console.log(true)
+console.log(1)
+console.log(-1)
+console.log("a")*/
+// Falsy o falsos
+/*console.log(0)
+console.log("")
+console.log(undefined)
+console.log(null)*/
+
+/* Control de flujo */
+// Condicionales
+/* if (true) {
+  console.log("se cumple la condicion 1")
+}
+
+if (false) {
+  console.log("se cumple la condicion 2")
+} else {
+  console.log("no se cumple la condicion 2")
+}
+
+if (false) {
+  console.log("se cumple la condicion 3 a")
+} else if (true) {
+  console.log("no se cumple la condicion 3 b")
+} else {
+  console.log("no se cumple la condicion 3")
+}
+
+if (false) {
+  console.log("se cumple la condicion 4")
+}
+if (true) {
+  console.log("se cumple la condicion 5")
+} */
+
+/*const perfil = "profesor"
+switch (perfil){
+  case "estudiante":
+    console.log("eres un estudiante")
+    break;
+    case "profesor":
+    console.log ("perfil no permitido")
+    break;
+    default:
+    console.log ("perfil no permitido")
+    break;
+}*/
